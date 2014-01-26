5568b7a2767eadf738fbf7caaa28bbd76f79e1be
dreamwieber <gregorywieber@gmail.com>
2014-01-26 15:21:01 -0800

    Initial Commit

---
+++ ToneDrum/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ToneDrum
+//
+//  Created by Gregory Wieber on 1/26/14.
+//  Copyright (c) 2014 Apposite. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ToneDrum/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ToneDrum
+//
+//  Created by Gregory Wieber on 1/26/14.
+//  Copyright (c) 2014 Apposite. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
